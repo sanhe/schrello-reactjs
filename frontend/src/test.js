8dcea984cce9218aa448d6fea9f9fa83d1e8a42a
Aliaksei Shautsou <shautsou@gmail.com>
2020-07-13 16:43:04 +0300

    Configure test

---
+++ frontend/src/test.js
@@ -0,0 +1,9 @@
+describe("true or false", () => {
+    it("true is true", () => {
+        expect(true).toEqual(true);
+    });
+
+    it("false is false", () => {
+        expect(false).toEqual(false);
+    });
+});
